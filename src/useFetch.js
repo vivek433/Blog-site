21db91a680c08af3a7445942bc96c9856bb1030e
vivek kumar pandey <vp444625@gmail.com>
2022-09-28 22:03:33 +0530

    First commit

--- src/useFetch.js
+++ src/useFetch.js
@@ -6,10 +6,11 @@ const useFetch=(url)=>
     const [error,setError]=useState(null);
     useEffect(()=>
     {const abortCont=new AbortController();
-        
+
        fetch(url,{signal:abortCont.signal}).then((res)=>
        {if(!res.ok)
         {
+            
             throw Error('could not fetch data for that resource')
         }
 return res.json()
